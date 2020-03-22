cee63401b152f90df47d32a9f915087a548f919d
oguseyin <oguseyin@sdvor.com>
2020-03-22 22:49:02 +0300

    refactor

---
+++ go.mod
@@ -0,0 +1,8 @@
+module src
+
+go 1.13
+
+require (
+	github.com/nfnt/resize v0.0.0-20180221191011-83c6a9932646
+	golang.org/x/sync v0.0.0-20190911185100-cd5d95a43a6e
+)
--- src/resizer.go
+++ src/resizer.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"fmt"
+	"golang.org/x/sync/errgroup"
 	"image"
 	"image/jpeg"
 	"image/png"
@@ -10,33 +12,26 @@ import (
 	"log"
 	"os"
 	"path/filepath"
-	"sync"
+	"time"
 
 	"github.com/nfnt/resize"
 )
 
-type ResizedImage struct {
+const (
+	extJpg = ".jpg"
+	extJpeg = ".jpeg"
+	extPng = ".png"
+)
+
+type Image struct {
 	Filename string
 	Ext      string
 	Width    uint
 	Height   uint
-	Data     *image.Image
+	Data     image.Image
 }
 
-// Channel for store resized images.
-var resized = make(chan ResizedImage)
-
-// Check slice contains string.
-func StringInSlice(s string, list []string) bool {
-	for _, b := range list {
-		if b == s {
-			return true
-		}
-	}
-	return false
-}
-
-func GetNewWidth(w int, sc int) uint {
+func width(w int, sc int) uint {
 	var nw uint
 	if sc < 0 {
 		nw = uint(-w / sc)
@@ -46,7 +41,7 @@ func GetNewWidth(w int, sc int) uint {
 	return nw
 }
 
-func GetNewHeight(h int, sc int) uint {
+func height(h int, sc int) uint {
 	var nh uint
 	if sc < 0 {
 		nh = uint(-h / sc)
@@ -56,187 +51,192 @@ func GetNewHeight(h int, sc int) uint {
 	return nh
 }
 
-// Get all image files and save to passed buffer.
-func GetImageFiles(dirname string, recurs bool, result *[]string) {
-	files, err := ioutil.ReadDir(dirname)
+type Resizer struct {
+	files []string
+	dir string
+	saveDir string
+	sc int
+	recursive bool
+
+	tasks chan Image
+}
+
+type ResizerOptions struct {
+	Dir string
+	SaveDir string
+	Scale int
+	Recursive bool
+}
+
+func NewResizer(ctx context.Context, opt ResizerOptions) (*Resizer, error) {
+	r := &Resizer{
+		dir: opt.Dir,
+		saveDir: opt.SaveDir,
+		sc: opt.Scale,
+		recursive: opt.Recursive,
+		files: make([]string, 0),
+		tasks: make(chan Image, 1),
+	}
+	if err := r.getImages(ctx); err != nil {
+		return nil, err
+	}
+	return r, nil
+}
+
+func (r *Resizer) getImages(ctx context.Context) error {
+	fileInfo, err := ioutil.ReadDir(r.dir)
 	if err != nil {
-		log.Fatalf("Error: %v", err)
-		os.Exit(1)
+		return err
 	}
 
-	// list of provided image extensions
-	providedExt := []string{".jpg", ".jpeg", ".png"}
-	for _, file := range files {
-		// configure path to file
-		absPath := filepath.Join(dirname, file.Name())
-		ext := filepath.Ext(absPath)
+	for _, file := range fileInfo {
+		ext := filepath.Ext(file.Name())
 
-		if recurs {
+		if r.recursive {
 			if file.IsDir() {
-				GetImageFiles(absPath, recurs, result)
+				if err := r.getImages(ctx); err != nil {
+					return err
+				}
 			} else {
-				// Check is file extension provided.
-				if StringInSlice(ext, providedExt) {
-					// Add path to file to result slice.
-					*result = append(*result, absPath)
+				if ext == extJpg || ext == extJpeg || ext == extPng {
+					r.files = append(r.files, filepath.Join(r.dir, file.Name()))
 				}
 			}
 		} else {
-			if !file.IsDir() {
-				if StringInSlice(ext, providedExt) {
-					*result = append(*result, absPath)
-				}
+			if ext == extJpg || ext == extJpeg || ext == extPng {
+				r.files = append(r.files, filepath.Join(r.dir, file.Name()))
 			}
 		}
 	}
+	return nil
 }
 
-// Parse command line arguments and return flag values.
-func ParseCommandLine() (bool, string, int, string) {
-	// configure command line flags
-	recursively := flag.Bool("r", false, "Sets for recursively obtain all images in directory.")
-	imgPath := flag.String("d", "", "Sets the path to dir containing images.")
-	scaling := flag.Int("sc", 1, "Set x times scaling value. Negative means downscaling. Positive upscaling.")
-	pathToSave := flag.String("p", *imgPath, "Sets the path to save resized images. Default value -d flag value.")
+func (r *Resizer) Resize(ctx context.Context) error {
+	var (
+		img  Image
+		source image.Image
+		file *os.File
 
-	// parse command line flags
-	flag.Parse()
+		err error
+	)
 
-	// return flag values
-	return *recursively, *imgPath, *scaling, *pathToSave
-}
-
-func Resize(files []string, scale int, wg *sync.WaitGroup) {
-
-	var resizedImg ResizedImage
-
-	for _, filename := range files {
-
-		// open file.
-		file, err := os.Open(filename)
+	for _, filename := range r.files {
+		file, err = os.Open(filename)
 		if err != nil {
-			log.Fatalf("97: Error: %v", err)
+			return err
 		}
 
 		ext := filepath.Ext(filename)
-		// decode jpeg into image.Image
-		if ext == ".jpeg" || ext == ".jpg" {
-			img, err := jpeg.Decode(file)
+		switch {
+		case ext == extJpeg || ext == extJpg:
+			source, err = jpeg.Decode(file)
 			if err != nil {
-				log.Fatalf("Error: %v", err)
+				return err
 			}
-
-			// Get image size.
-			width := GetNewWidth(img.Bounds().Max.X, scale)
-			height := GetNewHeight(img.Bounds().Max.Y, scale)
-
-			// resize using Lanczos resampling and preserve aspect ratio
-			m := resize.Resize(width, height, img, resize.Lanczos3)
-			resizedImg = ResizedImage{filename, ext, width, height, &m}
-
-			// decode png into image.Image
-		} else if ext == ".png" {
-			img, err := png.Decode(file)
+		case ext == extPng:
+			source, err = png.Decode(file)
 			if err != nil {
-				log.Fatalf("Error: %v", err)
+				return err
 			}
-
-			// Get image size.
-			width := GetNewWidth(img.Bounds().Max.X, scale)
-			height := GetNewHeight(img.Bounds().Max.Y, scale)
-
-			// resize using Lanczos resampling and preserve aspect ratio
-			m := resize.Resize(width, height, img, resize.Lanczos3)
-			resizedImg = ResizedImage{filename, ext, width, height, &m}
 		}
-		defer file.Close()
 
-		// send filename and resized image data to channel
-		resized <- resizedImg
+		width := width(source.Bounds().Max.X, r.sc)
+		height := height(source.Bounds().Max.Y, r.sc)
+
+		m := resize.Resize(width, height, source, resize.Lanczos3)
+		img = Image{
+			Filename: filename,
+			Ext: ext,
+			Width: width,
+			Height: height,
+			Data: m,
+		}
+		r.tasks <- img
 	}
-	// Decrement the counter when goroutine completes.
-	defer wg.Done()
+	defer func() {
+		_ = file.Close()
+		close(r.tasks)
+	}()
+
+	return nil
 }
 
-func HandleResized(savedir string, n int, wg *sync.WaitGroup) {
-
+func (r *Resizer) Save(ctx context.Context) error {
 	var (
 		err error
 		out *os.File
 	)
 
-	// Create savedir if not exist.
-	if _, e := os.Stat(savedir); os.IsNotExist(e) {
-		// Create directory with ModePerm = 0777 permissions.
-		os.Mkdir(savedir, os.ModePerm)
+	if _, err := os.Stat(r.saveDir); os.IsNotExist(err) {
+		if err := os.Mkdir(r.saveDir, 0644); err != nil {
+			return err
+		}
 	}
 
-	for i := 0; i < n; i++ {
-		// Get data from channel.
-		resizedImg := <-resized
+	for img := range r.tasks {
+		filename := fmt.Sprintf(
+			"%dx%d_%d.%s",
+			img.Width, img.Height, time.Since(time.Unix(0, time.Now().Unix())), img.Ext,
+		)
 
-		// Change initial image.
-		if savedir == "" {
-			out, err = os.Create(resizedImg.Filename)
-		} else {
-			// Get image size.
-			width := resizedImg.Width
-			height := resizedImg.Height
-
-			// Get file name from full path.
-			_, filename := filepath.Split(resizedImg.Filename)
-
-			// Configure new file name.
-			newFilename := fmt.Sprint(width) + "x" + fmt.Sprint(height) + "_" + filename
-			pathToSave := filepath.Join(savedir, newFilename)
-
-			// Create new file.
-			out, err = os.Create(pathToSave)
-		}
+		out, err = os.Create(filename)
 		if err != nil {
-			log.Fatalf("Error: %v", err)
+			return err
 		}
-		defer out.Close()
 
-		// Write resized image to newly created file.
-		if resizedImg.Ext == ".jpeg" || resizedImg.Ext == ".jpg" {
-			jpeg.Encode(out, *resizedImg.Data, nil)
-		} else if resizedImg.Ext == ".png" {
-			png.Encode(out, *resizedImg.Data)
+		switch {
+		case img.Ext == extJpeg || img.Ext == extJpg:
+			if err := jpeg.Encode(out, img.Data, nil); err != nil {
+				return err
+			}
+		case img.Ext == extPng:
+			if err := png.Encode(out, img.Data); err != nil {
+				return err
+			}
 		}
 	}
-	// Decrement the counter when goroutine completes.
-	defer wg.Done()
+	defer func() {
+		_ = out.Close()
+	}()
+
+	return nil
 }
 
 func main() {
-	// Get flag values.
-	recFlag, pathFlag, scaleFlag, savePath := ParseCommandLine()
-	if pathFlag == "" {
-		log.Fatalf("Error. Path to directory containing images required.")
+	var (
+		r       = flag.Bool("r", false, "Sets for recursively obtain all images in directory.")
+		sc      = flag.Int("sc", 1, "Set x times scaling value. Negative means downscaling. Positive up scaling.")
+		dir     = flag.String("d", "", "Sets the path to dir containing images.")
+		saveDir = flag.String("p", *dir, "Sets the path to save resized images. Default value -d flag value.")
+	)
+	flag.Parse()
+
+	if *dir == "" {
+		log.Fatalf("Path to directory containing images required")
 	}
-	if scaleFlag == 0 {
-		log.Fatalf("Error. Scale value should be different from zero.")
+	if *sc == 0 {
+		log.Fatalf("Scale value should be different from zero")
 	}
 
-	var files []string
-	// Get all image files and save it to files buffer.
-	GetImageFiles(pathFlag, recFlag, &files)
+	resizer, err := NewResizer(context.Background(), ResizerOptions{
+		Dir:       *dir,
+		SaveDir:   *saveDir,
+		Scale:     *sc,
+		Recursive: *r,
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
 
-	var wg sync.WaitGroup
-	// Increment the WaitGroup counter.
-	wg.Add(1)
-	// Resize images.
-	go Resize(files, scaleFlag, &wg)
+	eg, runCtx := errgroup.WithContext(context.Background())
+	eg.Go(func() error { return resizer.Resize(runCtx) })
+	go func() {
+		if err := eg.Wait(); err != nil {
+			log.Fatal(err)
+		}
+	}()
 
-	// Increment the WaitGroup counter.
-	wg.Add(1)
-	// Change name resized images and save to specific path.
-	go HandleResized(savePath, len(files), &wg)
-
-	// Wait for complete goroutines.
-	wg.Wait()
-
-	// Receive a message about completion.
-	log.Printf("Done. Resized %v img.", len(files))
+	if err := resizer.Save(runCtx); err != nil {
+		log.Fatal(err)
+	}
 }
